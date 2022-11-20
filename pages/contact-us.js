0aba2d4ab982e33582ec690edba3ba08438c7936
Pouryak <oldman2k19@gmail.com>
2022-11-20 22:07:53 +0330

    created about us page

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -4,7 +4,7 @@ import Main from "../src/components/layout/Main";
 const ContactUs = () => {
   return (
     <Main>
-      <div className="container mx-auto farsi-text">
+      <div className="container mx-auto bg-secondaryLight dark:bg-secondaryDark shadow-md rounded-md my-6 farsi-text">
         <p>Contact US</p>
       </div>
     </Main>
